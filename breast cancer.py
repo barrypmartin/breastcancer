48dfa6d3d267e05addb08756d91cbd42fb4e49c6
barrypmartin <125478596+barrypmartin@users.noreply.github.com>
2023-02-23 17:26:38 -0700

    Add files via upload

---
+++ breast cancer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 19:06:23 2023
+
+@author: barry
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
